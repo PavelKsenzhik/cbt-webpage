a1d178c556813a921bae5c49742fa22a472a84d4
P.Ksonzhik <P.Ksonzhik@cbt.by>
2022-09-29 15:08:22 +0300

    Made main-banner

---
+++ js/script.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+    window.addEventListener('scroll', function () {
+        let header = document.querySelector('.header');
+        let mainBanner = document.querySelector('.main-banner');
+
+        let bannerHeight = mainBanner.offsetHeight;
+        if(bannerHeight - window.pageYOffset < 0) {
+            header.classList.add('_scrolled')
+        }
+        if(bannerHeight - window.pageYOffset > 0) {
+            header.classList.remove('_scrolled')
+        }
+    })
+
+    $('a[href^="#"').on('click', function() {
+        let href = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        });
+        return false;
+    });
+
+
+    // ============= //
+    // Years counter //
+    // ============= //
+
+    function setYearsCount (startYear, maxYear) {
+        return function runYearsCount() {
+            setTimeout(() => {
+                startYear++;
+                if(startYear <= maxYear) {
+                    $('.main-banner__years-count').html(`${startYear}&nbsp;`);
+                    runYearsCount(startYear, maxYear);
+                }
+            }, 40)
+        }
+    }
+    
+    setTimeout(setYearsCount(0, 25), 100);
+
+}
